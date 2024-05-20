b5638caf84154eb4d2a65147b883a5cf16b65ddd
DanielClavel <7dcv2005@gmail.com>
2024-05-20 14:11:45 +0200

    organización del código

--- js/index.js
+++ js/index.js
@@ -23,13 +23,13 @@ opcionesMenu.forEach(opcion => {
 // Selecciona todos los elementos HTML que tienen la clase "input"
 const inputs = document.querySelectorAll(".input");
 
-//Hace el focus como de legend
+//Hace el focus
 function focusFunc() {
     let parent = this.parentNode;  // Obtiene el elemento padre del elemento actual
     parent.classList.add("focus");  // Añade la clase "focus" al elemento padre
 }
 
-//Hace el blur como de legend
+//Hace el blur
 function blurFunc() {
   let parent = this.parentNode;  // Obtiene el elemento padre del elemento actual
   // Si el campo está vacío, quita la clase "focus"
@@ -46,29 +46,19 @@ inputs.forEach((input) => {
     input.addEventListener("blur", blurFunc);
 });
 
-document.getElementById('sign-in').addEventListener('click', function() {
-    document.querySelector('.register').classList.add('hide');
-    document.querySelector('.login').classList.remove('hide');
-});
-
-document.getElementById('sign-up').addEventListener('click', function() {
-    document.querySelector('.login').classList.add('hide');
-    document.querySelector('.register').classList.remove('hide');
-});
-
 // Login
 const btnLogin = document.getElementById('sign-in');
 const btnRegistrarse = document.getElementById('sign-up');
 const formularioLogin = document.querySelector(".login");
 const formularioRegistrarse = document.querySelector(".register");
 
-
+// Al dar click en el botón de Login, oculta el formulario de registrarse y muestra el de login
 btnLogin.addEventListener("click", e => {
     formularioRegistrarse.classList.add("hide");
     formularioLogin.classList.remove("hide")
 })
 
-
+// Al dar click en el botón de registrarse, oculta el formulario de login y muestra el de registrarse
 btnRegistrarse.addEventListener("click", e => {
     formularioLogin.classList.add("hide");
     formularioRegistrarse.classList.remove("hide")
